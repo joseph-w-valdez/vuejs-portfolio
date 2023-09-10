cd1451fb6230b6d1844b7fc80c7b98a5f151aa7e
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-09-10 05:30:46 +0000

    adjusted project tiles to properly fit images in their frames

--- src/components/projects/ProjectSingleTile.vue
+++ src/components/projects/ProjectSingleTile.vue
@@ -12,8 +12,12 @@ export default {
     class="rounded-xl shadow-lg group cursor-pointer mb-10 sm:mb-0 relative"
     aria-label="Single Project"
   >
-    <div>
-      <img :src="project.thumbnail" :alt="project.title" class="rounded-t-xl border-none max-h-[227px]" />
+    <div class='h-[260px] w-[260px]'>
+      <img
+        :src="project.thumbnail"
+        :alt="project.title"
+        class="rounded-t-xl border-none object-cover w-full h-full"
+      />
     </div>
     <div class="text-center px-4 py-6 rounded-b-xl shadow-lg bg-secondary-light dark:bg-ternary-dark group-hover:bg-[#6366F1] ">
       <p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2 group-hover:text-white">
