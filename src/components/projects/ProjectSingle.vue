0157e40464ef8200c1687d7315362f888b3afd39
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-09-10 01:13:02 +0000

    added in hover effects and styling to project tiles

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -9,28 +9,22 @@ export default {
 <template>
   <router-link
     :to="`/projects/${project.id}`"
-    class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
+    class="rounded-xl shadow-lg group cursor-pointer mb-10 sm:mb-0 relative"
     aria-label="Single Project"
   >
     <div>
       <img :src="project.thumbnail" :alt="project.title" class="rounded-t-xl border-none" />
     </div>
-    <div class="text-center px-4 py-6">
-      <p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
+    <div class="text-center px-4 py-6 rounded-b-xl shadow-lg bg-secondary-light dark:bg-ternary-dark group-hover:bg-[#6366F1] ">
+      <p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2 group-hover:text-white">
         {{ project.title }}
       </p>
-      <span class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light">{{ project.category }}</span>
-
-      <!-- Display additional project details here -->
-      <div class="mt-4">
-        <h3 class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light">Date:</h3>
-        <p class="text-ternary-dark dark:text-ternary-light">{{ project.date}}</p>
-
-        <!-- You can add more details like client, objectives, technologies, etc. here -->
-      </div>
+      <span class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light group-hover:text-white">{{ project.category }}</span>
     </div>
   </router-link>
 </template>
+
+
 
 
 <style lang="scss" scoped></style>
