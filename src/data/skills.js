c10b20b79330db79d1f3565af2d17fc4e44f00b1
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-09-10 16:53:40 +0000

    updated skills data

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,8 +1,8 @@
 const skills = [
   {
     id: 1,
-    title: 'Hooks',
-    logo: 'https://github.com/devicons/devicon/raw/master/icons/react/react-original-wordmark.svg',
+    title: 'React Hooks',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/react/react-original.svg',
     website: 'https://reactjs.org/docs/hooks-intro.html'
   },
   {
@@ -31,46 +31,100 @@ const skills = [
   },
   {
     id: 6,
+    title: 'Express',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/express/express-original.svg',
+    website: 'https://expressjs.com/'
+  },
+  {
+    id: 7,
+    title: 'npm',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/npm/npm-original-wordmark.svg',
+    website: 'https://www.npmjs.com/'
+  },
+  {
+    id: 8,
+    title: 'Docker',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/docker/docker-original.svg',
+    website: 'https://www.docker.com/'
+  },
+  {
+    id: 9,
     title: 'VS Code',
     logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/vscode/vscode-original.svg',
     website: 'https://code.visualstudio.com/'
   },
   {
-    id: 7,
+    id: 10,
+    title: 'Vue.js',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/vuejs/vuejs-original.svg',
+    website: 'https://vuejs.org/'
+  },
+  {
+    id: 11,
     title: 'PostgreSQL',
     logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original.svg',
     website: 'https://www.postgresql.org/'
   },
   {
-    id: 8,
+    id: 12,
     title: 'HTML5',
     logo: 'https://github.com/devicons/devicon/raw/master/icons/html5/html5-original.svg',
     website: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'
   },
   {
-    id: 9,
+    id: 13,
     title: 'CSS3',
     logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original.svg',
     website: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
   },
   {
-    id: 10,
+    id: 14,
+    title: 'Firebase',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/firebase/firebase-plain.svg',
+    website: 'https://firebase.google.com/'
+  },
+  {
+    id: 15,
+    title: 'Socket.IO',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/socketio/socketio-original.svg',
+    website: 'https://socket.io/'
+  },
+  {
+    id: 16,
+    title: 'React Native',
+    logo: require('../assets/images/react-native-devicon.png'),
+    website: 'https://reactnative.dev/'
+  },
+  {
+    id: 17,
     title: 'Tailwind',
     logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/tailwindcss/tailwindcss-plain.svg',
+    website: 'https://mui.com/'
+  },
+  {
+    id: 18,
+    title: 'MaterialUI',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/materialui/materialui-plain.svg',
     website: 'https://tailwindcss.com/'
   },
   {
-    id: 11,
+    id: 19,
     title: 'Webpack',
     logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/webpack/webpack-original.svg',
     website: 'https://webpack.js.org/'
   },
   {
-    id: 12,
+    id: 20,
+    title: 'Figma',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/figma/figma-original.svg',
+    website: 'https://www.figma.com/'
+  },
+  {
+    id: 21,
     title: 'Photoshop',
     logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/photoshop/photoshop-plain.svg',
     website: 'https://www.adobe.com/products/photoshop.html'
   }
 ];
 
-export default skills
+export default skills;
