b4bc1dc6d0c6bc54d66edd8546f08e5cefd81810
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-09-11 19:05:11 +0000

    removed unused assets. changed download CV button to view CV button

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -28,36 +28,28 @@ export default {
 	>
 		<!-- Banner left contents -->
 		<div class="w-full md:w-1/3 text-left">
-			<h1
-				class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
-			>
+			<h1 class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase" >
 				Hi, I'm Joseph!
 			</h1>
-			<p
-				class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400"
-			>
+			<p class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400" >
 				Full-Stack Web Developer
 			</p>
+			<!-- CV button -->
 			<div class="flex justify-center sm:block">
 				<a
-					download="Joseph Valdez - Resume.pdf"
+					target='_blank'
 					href="/files/Joseph Valdez - Resume.pdf"
 					class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 					aria-label="Download Resume"
 				>
-					<i
-						data-feather="arrow-down-circle"
-						class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6 duration-100"
-					></i>
 					<span
-						class="text-sm sm:text-lg font-general-medium duration-100"
-						>Download CV</span
+						class="text-sm sm:text-lg font-general-medium duration-100!"
+						>View My Resume!</span
 					></a
 				>
 			</div>
-		</div>
 
-		<!-- Banner right illustration -->
+		</div>
 		<div class="flex justify-center items-center w-full md:w-2/3 text-right float-right">
 			<img
 				src="@/assets/images/profile.jpg"
