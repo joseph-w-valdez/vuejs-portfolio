ee74d0ad48e8bc654c8b65519ab9aa8be7bb6877
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-09-13 02:30:05 +0000

    switched back button to have a left arrow icon instead of the word back

--- src/components/projects/ProjectHeader.vue
+++ src/components/projects/ProjectHeader.vue
@@ -1,26 +1,26 @@
 <script>
-import Button from '../reusable/Button.vue';
+import 'feather-icons';
 
 export default {
-	components: { Button },
 	props: ['singleProjectHeader'],
 };
 </script>
 
 <template>
 	<div>
-		<div class="flex justify-between items-center mt-14 sm:mt-20 mb-7">
+		<div class="flex gap-8 justify-start items-center mt-14 sm:mt-20 mb-7">
+		<router-link to="/projects">
+        <button
+          class="h-10 w-10 text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-full hover:text-[#6366F1] hover:bg-white duration-500 flex items-center justify-center"
+          aria-label="Back"
+        >
+          <i data-feather="arrow-left"></i>
+        </button>
+      </router-link>
 			<p class="font-general-medium text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light " >
 				{{ singleProjectHeader.title }}
 			</p>
-			<router-link to="/projects">
-        <Button
-          title="Back"
-          class="h-10 w-16 text-white tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg hover:text-[#6366F1] hover:bg-white duration-500"
-          aria-label="Back"
-        >
-        </Button>
-      </router-link>
+
 		</div>
 		<div class="flex gap-4">
 			<div class="flex items-center">
